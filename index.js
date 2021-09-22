596e402942cbc3fa3bff7787cfb2bca8addf1754
Peter Fontana <dude0508@gmail.com>
2021-09-22 17:43:26 -0400

    commit tree be0d8624844bc894af766316391d2977ee702dfd

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+
+function findMatching(array, string) {
+    let matching = array.filter((names) => names.toLowerCase() === string.toLowerCase() )
+    return matching
+}
+
+function fuzzyMatch(array, string) {
+    let matchUp = array.filter((element) => (element[0] + element[1]).toLowerCase() === (string[0] + string[1]).toLowerCase() )
+    return matchUp
+}
+
+function matchName(array, string) {
+    let matching = array.filter((element) => element.name.toLowerCase() === string.toLowerCase())
+    return matching
+}
